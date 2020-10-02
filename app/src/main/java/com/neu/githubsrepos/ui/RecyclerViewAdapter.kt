618e01f34388db190c45351383be9a4060922f27
Irineu333 <mmmirineusilva@gmail.com>
2020-10-02 13:24:08 -0300

    Carregando os avatares com Glide

--- app/src/main/java/com/neu/githubsrepos/ui/RecyclerViewAdapter.kt
+++ app/src/main/java/com/neu/githubsrepos/ui/RecyclerViewAdapter.kt
@@ -9,10 +9,11 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.neu.githubsrepos.R
 import com.neu.githubsrepos.github.models.Repository
 
-class RecyclerViewAdapter(private val context: Context,  val listaRepos : MutableList<Repository> = mutableListOf()) : RecyclerView.Adapter<RecyclerViewAdapter.Holder>() {
+class RecyclerViewAdapter(private val context: Context, private val listaRepos : MutableList<Repository> = mutableListOf()) : RecyclerView.Adapter<RecyclerViewAdapter.Holder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_view, parent, false)
@@ -25,15 +26,15 @@ class RecyclerViewAdapter(private val context: Context,  val listaRepos : Mutabl
     }
     override fun onBindViewHolder(holder: Holder, position: Int) {
         //objects
-        val repository : Repository = listaRepos.get(position)
+        val repository : Repository = listaRepos[position]
         val autor = repository.owner
 
         //strings
         val userName = repository.name
         val userLogin = autor.login
 
-        //avatar
-        //val avatar : Bitmap
+        //avatar config using Glide
+        Glide.with(context).load(repository.owner.avatar_url).placeholder(R.drawable.ic_image).into(holder.avatar)
 
         Log.d("RecyclerView", "bind view\n pos $position")
         Log.d("RecyclerView", "repo $userName, $userLogin")
@@ -41,11 +42,10 @@ class RecyclerViewAdapter(private val context: Context,  val listaRepos : Mutabl
     }
 
     override fun getItemCount(): Int {
-        Log.d("RecyclerView", "getItemCount ${listaRepos.size}")
         return listaRepos.size
     }
 
-    fun setRepositories(listaRepos : MutableList<Repository>)
+    fun setRepositories(listaRepos : List<Repository>)
     {
         this.listaRepos.clear()
         listaRepos.forEach{ this.listaRepos.add(it) }
@@ -56,14 +56,14 @@ class RecyclerViewAdapter(private val context: Context,  val listaRepos : Mutabl
 
     class Holder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {
-        private val avatar = itemView.findViewById<ImageView>(R.id.avatar)
+        val avatar: ImageView = itemView.findViewById(R.id.avatar)
         private val userName = itemView.findViewById<TextView>(R.id.user_name)
         private val loginName = itemView.findViewById<TextView>(R.id.login_name)
 
         /** Vincula as informações nas views corretas*/
         fun bind(avatar: Bitmap?, userName : String, loginName : String)
         {
-            this.avatar?.setImageBitmap(avatar)
+            this.avatar.setImageBitmap(avatar)
             this.userName.text = userName
             this.loginName.text = loginName
         }
