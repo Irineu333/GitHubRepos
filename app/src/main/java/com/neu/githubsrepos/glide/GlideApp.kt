8513984efeead44e5f5d7226f5d3a1c3c865cfc2
Irineu333 <mmmirineusilva@gmail.com>
2020-10-05 14:14:21 -0300

    Implementação da classe AppGlideMudule com annotation @GlideModule para funcionamento adequado do Glide

---
+++ app/src/main/java/com/neu/githubsrepos/glide/GlideApp.kt
@@ -0,0 +1,9 @@
+package com.neu.githubsrepos.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideApp : AppGlideModule() {
+
+}
