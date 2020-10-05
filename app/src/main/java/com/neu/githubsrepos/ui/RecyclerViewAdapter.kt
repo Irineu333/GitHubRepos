467c98bcd56e683b48ccdaa9ecacc1b6a0d5b1ea
Irineu333 <mmmirineusilva@gmail.com>
2020-10-05 14:20:29 -0300

    Implementação de Listener para comnunicação com as activities

--- app/src/main/java/com/neu/githubsrepos/ui/RecyclerViewAdapter.kt
+++ app/src/main/java/com/neu/githubsrepos/ui/RecyclerViewAdapter.kt
@@ -1,7 +1,6 @@
 package com.neu.githubsrepos.ui
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -13,10 +12,15 @@ import com.bumptech.glide.Glide
 import com.neu.githubsrepos.R
 import com.neu.githubsrepos.github.models.Repository
 
-class RecyclerViewAdapter(private val context: Context, private val listaRepos : MutableList<Repository> = mutableListOf()) : RecyclerView.Adapter<RecyclerViewAdapter.Holder>() {
+class RecyclerViewAdapter(
+    val context: Context,
+    private var listaRepos: MutableList<Repository> = mutableListOf(),
+    val listener: OnClickListener
+) : RecyclerView.Adapter<RecyclerViewAdapter.Holder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_view, parent, false)
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_view, parent, false)
         Log.d("RecyclerView", "create view")
         return Holder(itemView)
     }
@@ -24,49 +28,70 @@ class RecyclerViewAdapter(private val context: Context, private val listaRepos :
     init {
         Log.d("RecyclerView", "count ${listaRepos.size}")
     }
+
     override fun onBindViewHolder(holder: Holder, position: Int) {
         //objects
-        val repository : Repository = listaRepos[position]
+        val repository: Repository = listaRepos[position]
         val autor = repository.owner
 
         //strings
         val userName = repository.name
         val userLogin = autor.login
 
-        //avatar config using Glide
-        Glide.with(context).load(repository.owner.avatar_url).placeholder(R.drawable.ic_image).into(holder.avatar)
+        holder.languages.text = ""
+        repository.getLanguages(holder.languages)
 
-        Log.d("RecyclerView", "bind view\n pos $position")
+        //avatar config using Glide
+        Glide.with(context).load(repository.owner.avatar_url).placeholder(R.drawable.ic_perfil)
+            .into(holder.avatar)
+
+        Log.d("RecyclerView", "bind view pos $position")
         Log.d("RecyclerView", "repo $userName, $userLogin")
-        holder.bind(null, userName, userLogin)
+
+
+        holder.itemView.setOnClickListener {
+
+            listener.onClick(repository)
+        }
+
+        holder.bind(userName, userLogin)
     }
 
     override fun getItemCount(): Int {
         return listaRepos.size
     }
 
-    fun setRepositories(listaRepos : List<Repository>)
-    {
+    fun setRepositories(listaRepos: MutableList<Repository>) {
         this.listaRepos.clear()
-        listaRepos.forEach{ this.listaRepos.add(it) }
+        this.listaRepos = listaRepos
         notifyDataSetChanged()
 
         Log.d("RecyclerView", "count ${this.listaRepos.size}")
     }
 
-    class Holder(itemView: View) : RecyclerView.ViewHolder(itemView)
-    {
+    class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private var onClickListener : View.OnClickListener? = null
+        private var position : Int? = null
+
         val avatar: ImageView = itemView.findViewById(R.id.avatar)
+        val languages = itemView.findViewById<TextView>(R.id.languages)
+
         private val userName = itemView.findViewById<TextView>(R.id.user_name)
-        private val loginName = itemView.findViewById<TextView>(R.id.login_name)
+        private val login = itemView.findViewById<TextView>(R.id.login)
 
         /** Vincula as informações nas views corretas*/
-        fun bind(avatar: Bitmap?, userName : String, loginName : String)
-        {
-            this.avatar.setImageBitmap(avatar)
+        fun bind(userName: String, login: String) {
             this.userName.text = userName
-            this.loginName.text = loginName
+            this.login.text = login
         }
+
+    }
+
+
+    interface  OnClickListener {
+
+        fun onClick(data:Any)
     }
 
 }
