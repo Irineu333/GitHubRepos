769004901b05a9b7d06d7b15bb96ed2ad9b224aa
Irineu333 <mmmirineusilva@gmail.com>
2020-10-05 14:16:14 -0300

    Adicionado função listLanguages na interface GitHubService.kt

--- app/src/main/java/com/neu/githubsrepos/github/GitHubService.kt
+++ app/src/main/java/com/neu/githubsrepos/github/GitHubService.kt
@@ -1,14 +1,19 @@
 package com.neu.githubsrepos.github
 
 import com.neu.githubsrepos.github.models.Repository
+
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface GitHubService {
 
-    /** Restona os repositórios publicos, quantidade=100 */
+    /** repositórios publicos */
     @GET("/repositories")
     fun listPublic() : Call<List<Repository>>
+
+    /** Languages de um repositório */
+    @GET("/repos/{login}/{name}/languages")
+    fun listLanguages(@Path("login") login : String, @Path("name") name : String) : Call<HashMap<String, Any>>
+
 }
